08690049341e335c4c461cfd0ccfb6bba1381dff
Hugo Dias <hugomrdias@gmail.com>
2018-10-23 04:05:20 +0100

    feat: add everything for ci and renovate

--- .aegir.js
+++ .aegir.js
@@ -21,7 +21,46 @@ module.exports = {
       included: false
     }],
     browserNoActivityTimeout: 100 * 1000,
-    singleRun: true
+    singleRun: true,
+    reporters: ['coverage-istanbul'],
+    coverageIstanbulReporter: {
+      reports: ['json', 'html', 'text-summary'],
+      dir: path.join(__dirname, 'coverage'),
+      combineBrowserReports: true,
+      fixWebpackSourcePaths: true,
+      'report-config': {
+        html: {
+          subdir: 'html'
+        }
+      },
+    },
+    preprocessors:
+   { 'node_modules/aegir/src/config/karma-entry.js': [ 'webpack', 'sourcemap' ] },
+    webpack: {
+      module: {
+        rules: [
+          // instrument only testing sources with Istanbul
+          {
+            test: /\.js$/,
+            use: { loader: 'istanbul-instrumenter-loader' },
+            include: path.resolve('src/')
+          }
+        ]
+      }
+    },
+    customLaunchers: {
+      ChromeDocker: {
+        base: 'ChromeHeadless',
+        // We must disable the Chrome sandbox when running Chrome inside Docker (Chrome's sandbox needs
+        // more permissions than Docker allows by default)
+        flags: ['--no-sandbox']
+      }
+    },
+    client: {
+      mocha: {
+        bail: true,
+      }
+    },
   },
   hooks: {
     node: {
